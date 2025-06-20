fc378d9123a7314e675c237ab7bb607d808a1ba3
Achie <achraf_ben.az@hotmail.com>
2025-06-21 01:28:59 +0200

    Books can now be stored in the cart and you can increase their quantity

--- src/main/java/ucd/bookstore/repository/CartItemRepository.java
+++ src/main/java/ucd/bookstore/repository/CartItemRepository.java
@@ -4,12 +4,12 @@ package ucd.bookstore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ucd.bookstore.model.Cart;
+import ucd.bookstore.model.CartItem;
 import ucd.bookstore.model.User;
 
 import java.util.Optional;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUser(User user);
-    Optional<Cart> findById(Long id);
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findById(Long id);
 }
